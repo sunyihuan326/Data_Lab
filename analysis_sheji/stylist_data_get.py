c66bee4741376f5ce6d0ba42510f397ff7ab8a77
sunyihuan <syh2685@126.com>
2018-12-12 16:37:17 +0800

    stylist_data

--- analysis_sheji/stylist_data_get.py
+++ analysis_sheji/stylist_data_get.py
@@ -7,6 +7,8 @@ created on 2018/11/10
 from pymongo import MongoClient
 import time
 from bson.objectid import ObjectId
+import datetime
+import tqdm
 
 mdbs = MongoClient('dds-bp1c30e6691173a41935-pub.mongodb.rds.aliyuncs.com', 3717,
                    unicode_decode_error_handler='ignore')  # 链接mongodb
@@ -28,6 +30,15 @@ def id2ObjectId(_id):
         return int(_id)
     else:
         return ObjectId(_id)
+
+
+def ts2utcdatetime(ts):
+    '''
+    时间戳转化为日期，支持mongodb中的日期保存
+    :param ts:
+    :return:
+    '''
+    return datetime.datetime.utcfromtimestamp(ts)
 
 
 def get_cut_styles_distribution():
@@ -129,20 +140,21 @@ def work_years_numbers(work_years_count):
     count_2645 = 0
     count_46 = 0
     for k in work_years_count.keys():
-        if int(k) > 0 and int(k) <= 3:
-            count_13 = count_13 + work_years_count[k]
-        elif int(k) > 3 and int(k) <= 5:
-            count_45 += work_years_count[k]
-        elif int(k) > 5 and int(k) <= 9:
-            count_69 += work_years_count[k]
-        elif int(k) > 10 and int(k) <= 15:
-            count_1015 += work_years_count[k]
-        elif int(k) > 16 and int(k) <= 25:
-            count_1625 += work_years_count[k]
-        elif int(k) > 25 and int(k) <= 45:
-            count_2645 += work_years_count[k]
-        else:
-            count_46 += work_years_count[k]
+        if k != "":
+            if int(k) > 0 and int(k) <= 3:
+                count_13 = count_13 + work_years_count[k]
+            elif int(k) > 3 and int(k) <= 5:
+                count_45 += work_years_count[k]
+            elif int(k) > 5 and int(k) <= 9:
+                count_69 += work_years_count[k]
+            elif int(k) > 10 and int(k) <= 15:
+                count_1015 += work_years_count[k]
+            elif int(k) > 16 and int(k) <= 25:
+                count_1625 += work_years_count[k]
+            elif int(k) > 25 and int(k) <= 45:
+                count_2645 += work_years_count[k]
+            else:
+                count_46 += work_years_count[k]
     return count_13, count_45, count_69, count_1015, count_1625, count_2645, count_46
 
 
@@ -232,3 +244,140 @@ def work_time_distribution():
             print(s["work_start"])
     return work_end_count
 
+
+def get_wangluosheji_stylists(day_time):
+    '''
+    获取某段时间内，完成网络设计订单的发型师名单
+    :param day_time:
+    :return:
+    '''
+    start_time = day_time + " 00:00:00"
+    start_timeArray = time.strptime(start_time, "%Y-%m-%d %H:%M:%S")
+    star_timeStamp = int(time.mktime(start_timeArray))
+    needs_finish = mdb.xm_hair_scheme.find({"is_finish": 1, "ctime": {"$gte": ts2utcdatetime(star_timeStamp),
+                                                                      "$lt": ts2utcdatetime(
+                                                                          star_timeStamp + 30 * 86400)}})
+    wangluo_stylist = []
+    for n_f in needs_finish:
+        need_id = id2ObjectId(n_f["need_id"])
+        needs_p = mdb.xm_hair_need.find({"_id": need_id})
+        for n in needs_p:
+            if n["source"] == 2:
+                if n["myid"] not in wangluo_stylist:
+                    wangluo_stylist.append(n["myid"])
+    return wangluo_stylist
+
+
+def wangluosheji_work_years_distribution(day_time):
+    '''
+    完成网络设计订单的发型师工作年限分布
+    :param day_time:
+    :return:
+    '''
+    wangluo_stylist = get_wangluosheji_stylists(day_time)
+    print(len(wangluo_stylist))
+    work_years_count = {}
+    for k in wangluo_stylist:
+        s_p = mdb.person.find({"uid": k})
+        for s_ in s_p:
+            wy = s_["work_year"]
+            if wy not in work_years_count.keys():
+                work_years_count[wy] = 1
+            else:
+                work_years_count[wy] = work_years_count[wy] + 1
+
+    return work_years_count
+
+
+def wangluo_cut_price_distributiom(day_time):
+    '''
+    完成网络设计订单的发型师剪发价格区间
+    :param day_time:
+    :return:
+    '''
+    wangluo_stylist = get_wangluosheji_stylists(day_time)
+    print("get_wangluo_stylist")
+    cut_price_count = {}
+    cut_price_count["1-38"] = 0
+    cut_price_count["39-58"] = 0
+    cut_price_count["59-100"] = 0
+    cut_price_count["101-180"] = 0
+    cut_price_count["181-280"] = 0
+    cut_price_count["281-480"] = 0
+    cut_price_count["481-"] = 0
+    for k in wangluo_stylist:
+        s_p = mdb.person.find({"uid": k})
+        for s_ in s_p:
+            wy = s_["cut_price"]
+            if wy != "" and wy >= 1 and wy <= 38:
+                cut_price_count["1-38"] += 1
+            if wy != "" and wy >= 39 and wy <= 58:
+                cut_price_count["39-58"] += 1
+            if wy != "" and wy >= 59 and wy <= 100:
+                cut_price_count["59-100"] += 1
+            if wy != "" and wy >= 101 and wy <= 180:
+                cut_price_count["101-180"] += 1
+            if wy != "" and wy >= 181 and wy <= 280:
+                cut_price_count["181-280"] += 1
+            if wy != "" and wy >= 281 and wy <= 480:
+                cut_price_count["281-480"] += 1
+            if wy != "" and wy >= 481:
+                cut_price_count["481-"] += 1
+    return cut_price_count
+
+
+def get_wangluosheji_cut_work_distribution(day_time):
+    '''
+    完成网络设计订单发型师的剪发价格、工作年限分布
+    :param day_time:
+    :return:
+    '''
+    wangluo_stylist = get_wangluosheji_stylists(day_time)
+    print("get_wangluo_stylist")
+    work_years_count = {}
+    cut_price_count = {}
+    cut_price_count["1-38"] = 0
+    cut_price_count["39-58"] = 0
+    cut_price_count["59-100"] = 0
+    cut_price_count["101-180"] = 0
+    cut_price_count["181-280"] = 0
+    cut_price_count["281-480"] = 0
+    cut_price_count["481-"] = 0
+    for k in wangluo_stylist:
+        s_p = mdb.person.find({"uid": k})
+        for s_ in s_p:
+            wy = s_["work_year"]
+            cp = s_["cut_price"]
+            if wy not in work_years_count.keys():
+                work_years_count[wy] = 1
+            else:
+                work_years_count[wy] = work_years_count[wy] + 1
+
+            if cp != "" and cp >= 1 and cp <= 38:
+                cut_price_count["1-38"] += 1
+            if cp != "" and cp >= 39 and cp <= 58:
+                cut_price_count["39-58"] += 1
+            if cp != "" and cp >= 59 and cp <= 100:
+                cut_price_count["59-100"] += 1
+            if cp != "" and cp >= 101 and cp <= 180:
+                cut_price_count["101-180"] += 1
+            if cp != "" and cp >= 181 and cp <= 280:
+                cut_price_count["181-280"] += 1
+            if cp != "" and cp >= 281 and cp <= 480:
+                cut_price_count["281-480"] += 1
+            if cp != "" and cp >= 481:
+                cut_price_count["481-"] += 1
+    return work_years_count, cut_price_count
+
+
+if __name__ == "__main__":
+    day_time = "2018-11-11"
+    # work_years_count = wangluosheji_work_years_distribution(day_time)
+    # print(work_years_count)
+    # print(work_years_numbers(work_years_count))
+    #
+    # cut_price_count = wangluo_cut_price_distributiom(day_time)
+    # print(cut_price_count)
+    work_years_count, cut_price_count = get_wangluosheji_cut_work_distribution(day_time)
+    print(cut_price_count)
+    print(work_years_numbers(work_years_count))
