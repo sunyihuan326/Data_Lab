682e80a47c0284c15d063826a983c29b944fa76a
sunyihuan <syh2685@126.com>
2019-01-14 18:44:59 +0800

    page uv

--- analysis_sheji/days_no_using.py
+++ analysis_sheji/days_no_using.py
@@ -4,10 +4,24 @@ created on 2019/1/4
 
 @author:sunyihuan
 '''
+import xlwt
+import tqdm
 from utils import connect_mongodb_sheji, connect_es, day2timestamp, list_change_type
 
 es = connect_es()
 mdb = connect_mongodb_sheji()
+
+
+def timeStamp2date(timeStamp):
+    '''
+    时间戳转化为日期
+    :param timeStamp:
+    :return:
+    '''
+    import time
+    timeArray = time.localtime(timeStamp)
+    otherStyleTime = time.strftime("%Y--%m--%d %H:%M:%S", timeArray)
+    return str(otherStyleTime).split(" ")[0]
 
 
 def get_vip_list(start_time):
@@ -70,10 +84,9 @@ def get_days_uv_list(end_time, day_nums, index):
     return uv_days_list
 
 
-def day_nums_no_using(days_nums):
+def day_nums_no_using(end_time, days_nums):
     index_release = 'user_behavior_log_prod_20180723'
-    end_time = "2019-1-7"
-    end_time = day2timestamp(end_time)
+
     uv_list = get_days_uv_list(end_time, days_nums, index_release)
 
     vip_list = get_vip_list(end_time)
@@ -84,11 +97,75 @@ def day_nums_no_using(days_nums):
     return no_using_list
 
 
+def Correcting_user(no_using_list, end_time, days_nums):
+    wx_user = mdb.wxuser.distinct("_id", {"mobile": {"$exists": True}})
+    p_user = mdb.person.distinct("uid", {"user_name": {"$exists": True, "$ne": ""}})
+    o_user = mdb.wx_order.distinct("uid", {"utime": {"$lt": end_time - 86400 * days_nums}})
+    no_using = set(list_change_type(str, wx_user)) & set(no_using_list) & set(list_change_type(str, o_user)) & set(
+        list_change_type(str, p_user))
+    return no_using
+
+
+def write2excel(no_using_list):
+    w = xlwt.Workbook()
+    sh = w.add_sheet("未使用发型师名单", cell_overwrite_ok=True)
+    sh.write(0, 0, "编号")
+    sh.write(0, 1, "名字")
+    sh.write(0, 2, "电话")
+    sh.write(0, 3, "最近一次购习vip时间")
+    sh.write(0, 4, "花费金额")
+    sh.write(0, 5, "会员到期时间")
+    c = 0
+    for i, st in enumerate(no_using_list):
+
+        myid = int(st)
+
+        try:
+            user = mdb.wxuser.find_one({"_id": myid})
+            try:
+                name = user["nickname"]
+            except:
+                name = mdb.person.find_one({"uid": myid})["user_name"]
+            tell = user["mobile"]
+
+            ex_time = user["expireat"]
+            ex_time = timeStamp2date(ex_time)
+
+            import pymongo
+            order = mdb.wx_order.find({"uid": myid, "type": {"$ne": "mc"}, "status": 1}).sort("_id",
+                                                                                              pymongo.DESCENDING).limit(
+                1)
+            try:
+                for o in order:
+                    buy_time = o["utime"]
+                    price = o["price"]
+                buy_time = timeStamp2date(buy_time)
+            except:
+                continue
+            if int(tell) > 0:
+                sh.write(i + 1, 0, i + 1)
+                sh.write(i + 1, 1, name)  # 写入名字
+                sh.write(i + 1, 2, tell)  # 写入手机号码
+                sh.write(i + 1, 3, buy_time)  # 写入购买时间
+                sh.write(i + 1, 4, price)  # 写入购买金额
+                sh.write(i + 1, 5, ex_time)  # 写入到期时间
+        except:
+            print(i, myid)
+            c += 1
+            print(c)
+
+    w.save("/Users/sunyihuan/Desktop/近30天未使用app名单.xls")
+
+
 if __name__ == "__main__":
-    days_nums = 7
-    no_using_list_7 = day_nums_no_using(7)
-    no_using_list_15 = day_nums_no_using(15)
-    no_using_list_30 = day_nums_no_using(30)
-    print("7日未登陆app人数:", len(no_using_list_7))
-    print("15日未登陆app人数:", len(no_using_list_15))
+    end_time = "2019-1-14"
+    end_time = day2timestamp(end_time)
+    # no_using_list_7 = day_nums_no_using(7)
+    # no_using_list_15 = day_nums_no_using(15)
+    no_using_list_30 = day_nums_no_using(end_time, 30)
+    no_using = Correcting_user(no_using_list_30, end_time, 30)
+    print(len(no_using))
+    # print("7日未登陆app人数:", len(no_using_list_7))
+    # print("15日未登陆app人数:", len(no_using_list_15))
     print("30日未登陆app人数:", len(no_using_list_30))
+    write2excel(no_using)
--- model_for_wangluo_design/data_from_hair_need.py
+++ model_for_wangluo_design/data_from_hair_need.py
@@ -18,7 +18,7 @@ xianchang_counts = mdb.xm_hair_need.find(
     {"status": 101, "source": 1}).count()
 
 
-def writeHead(sh):
+def writeHead_wangluo(sh):
     sh.write(0, 2, "three_court")
     sh.write(0, 3, "face_length")
     sh.write(0, 4, "face_sense")
@@ -43,15 +43,31 @@ def writeHead(sh):
     sh.write(0, 23, "hair_length")
     sh.write(0, 0, "id")
     sh.write(0, 1, "age")
+    sh.write(0, 24, "career")
 
 
-def write2excel():
+def writeHead_xianchang(sh):
+    sh.write(0, 0, "id")
+    sh.write(0, 1, "age")
+    sh.write(0, 2, "three_court")
+    sh.write(0, 3, "face_length")
+    sh.write(0, 4, "face_sense")
+    sh.write(0, 5, "face_shape")
+    sh.write(0, 6, "skin_shade")
+    sh.write(0, 7, "natural_style")
+    sh.write(0, 8, "shape")
+    sh.write(0, 9, "hair_length")
+    sh.write(0, 10, "skin_hue")
+    sh.write(0, 11, "eyelid")
+
+
+def write2excel_wangluo():
     wangluo = mdb.xm_hair_need.find(
         {"status": 101, "source": 2, "ctime": {"$gt": ts2utcdatetime(1538323200)}}).sort("_id", pymongo.DESCENDING)
 
     w = xlwt.Workbook()
     sh = w.add_sheet("wangluo_design0", cell_overwrite_ok=True)
-    writeHead(sh)
+    writeHead_wangluo(sh)
     for i, l in enumerate(wangluo):
         if i > 65530:
             break
@@ -72,6 +88,7 @@ def write2excel():
                 sh.write(i + 1, 21, l["hair_info"]["skin_hue"])
                 sh.write(i + 1, 22, l["hair_info"]["skin_shade"])
                 sh.write(i + 1, 23, l["hair_info"]["hair_length"])
+                sh.write(i + 1, 24, l["hair_info"]["career"])
 
                 sh.write(i + 1, 10, l["hair_info"]["like_change"])
                 sh.write(i + 1, 11, l["hair_info"]["like_length"])
@@ -88,7 +105,39 @@ def write2excel():
             except:
                 pass
 
-    w.save("/Users/sunyihuan/Desktop/wangluo2222222.xls")
+    w.save("/Users/sunyihuan/Desktop/wangluo.xls")
+
+
+def write2excel_xianchang():
+    xianchang = mdb.xm_hair_need.find(
+        {"status": 101, "source": 1}).sort("_id", pymongo.DESCENDING)
+
+    w = xlwt.Workbook()
+    sh = w.add_sheet("xianchang_design0", cell_overwrite_ok=True)
+    writeHead_xianchang(sh)
+    for i, l in enumerate(xianchang):
+        if i > 65530:
+            break
+        else:
+            id = str(l["_id"])
+            info = l["hair_info"]
+            age = info["age"]
+            natural_style = info["natural_style"]
+
+            sh.write(i + 1, 0, id)
+            sh.write(i + 1, 1, age)
+            sh.write(i + 1, 2, info["three_court"])
+            sh.write(i + 1, 3, info["face_length"])
+            sh.write(i + 1, 4, info["face_sense"])
+            sh.write(i + 1, 5, info["face_shape"])
+            sh.write(i + 1, 6, info["skin_shade"])
+            sh.write(i + 1, 7, natural_style)
+            sh.write(i + 1, 8, info["shape"])
+            sh.write(i + 1, 9, info["hair_length"])
+            sh.write(i + 1, 10, info["skin_hue"])
+            sh.write(i + 1, 11, info["eyelid"])
+
+    w.save("/Users/sunyihuan/Desktop/xianchang.xls")
 
 
 if __name__ == "__main__":
@@ -96,4 +145,5 @@ if __name__ == "__main__":
     print("现在设计总单数：", xianchang_counts)
     print("总单数：", wangluo_counts + xianchang_counts)
 
-    write2excel()
+    # write2excel_wangluo()
+    write2excel_xianchang()
